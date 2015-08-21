5d07d048f6bee21bf2719af4ef79bfc54e5bba9d
Molly Waggett <mwaggett@alumni.reed.edu>
2015-08-21 13:40:32 -0700

    Edit AppTest to import static org.fluentlenium.core.filter.FilterConstructor.*; add @Override (whatever that does..).

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,12 +5,15 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
 
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
   public WebDriver getDefaultDriver() {
       return webDriver;
   }
