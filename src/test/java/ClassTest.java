39031c5118aa403f7641d51c9215e5ba49c190a4
Molly Waggett <mwaggett@alumni.reed.edu>
2015-08-24 19:18:43 -0700

    Remove ClearRule altogether.

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -7,9 +7,6 @@ public class ClassTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
-  // @Rule
-  // public ClearRule clearRule = new ClearRule();
-
   // ~~UNIT TESTING~~
   // @Test
   // public void methodName_whatIsBeingTested_desiredResult() {
